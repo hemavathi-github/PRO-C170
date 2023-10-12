7c4849bd678be822523681b4db34db5efc5a9459
hemavathi-github <75414397+hemavathi-github@users.noreply.github.com>
2023-10-12 11:56:34 +0530

    Update addMarkers.js

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -32,24 +32,10 @@ AFRAME.registerComponent("create-markers", {
       marker.appendChild(model);
 
       // Ingredients Container
-      var mainPlane = document.createElement("a-plane");
-      mainPlane.setAttribute("id", `main-plane-${dish.id}`);
-      mainPlane.setAttribute("position", { x: 0, y: 0, z: 0 });
-      mainPlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
-      mainPlane.setAttribute("width", 1.7);
-      mainPlane.setAttribute("height", 1.5);
-      marker.appendChild(mainPlane);
+     
 
       // Dish title background plane
-      var titlePlane = document.createElement("a-plane");
-      titlePlane.setAttribute("id", `title-plane-${dish.id}`);
-      titlePlane.setAttribute("position", { x: 0, y: 0.89, z: 0.02 });
-      titlePlane.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-      titlePlane.setAttribute("width", 1.69);
-      titlePlane.setAttribute("height", 0.3);
-      titlePlane.setAttribute("material", { color: "#F0C30F" });
-      mainPlane.appendChild(titlePlane);
-
+     
       // Dish title
       var dishTitle = document.createElement("a-entity");
       dishTitle.setAttribute("id", `dish-title-${dish.id}`);
@@ -66,18 +52,7 @@ AFRAME.registerComponent("create-markers", {
       titlePlane.appendChild(dishTitle);
 
       // Ingredients List
-      var ingredients = document.createElement("a-entity");
-      ingredients.setAttribute("id", `ingredients-${dish.id}`);
-      ingredients.setAttribute("position", { x: 0.3, y: 0, z: 0.1 });
-      ingredients.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-      ingredients.setAttribute("text", {
-        font: "monoid",
-        color: "black",
-        width: 2,
-        align: "left",
-        value: `${dish.ingredients.join("\n\n")}`
-      });
-      mainPlane.appendChild(ingredients);
+      
     });
   },
   //function to get the dishes collection from firestore database
